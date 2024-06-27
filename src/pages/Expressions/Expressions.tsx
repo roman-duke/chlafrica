5fdaf91962da9f544ff9a7311083deec1470baed
Donald Roman <romanunuose@gmail.com>
2024-06-27 10:17:41 +0100

    Hid the Expressions intro section

--- src/pages/Expressions/Expressions.tsx
+++ src/pages/Expressions/Expressions.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ExpressionsIntro from "@components/Intro/ExpressionsIntro"
+// import ExpressionsIntro from "@components/Intro/ExpressionsIntro"
 import MainExpressions from "@components/Expressions/MainExpressions";
 
 export default function Expressions() {
@@ -11,7 +11,7 @@ export default function Expressions() {
       <div className="no-scrollbar max-w-[950px] m-auto">
         <section className="pt-3 lg:pt-5 px-10">
           <>
-            <h2 className="font-semibold text-brand-accent text-2xl lg:text-3xl text-end">Expressions</h2>
+            <h2 className="font-semibold text-brand-accent text-2xl lg:text-3xl text-center">CHL Expressions</h2>
             <p className="mt-2 text-base lg:text-lg font-semibold text-justify text-gray-400 leading-relaxed">
               We have curated insightful articles, exciting upcoming events, and impactful project 
               stories to stay informed, equipped and engaged with CHL Africa's Afrochamp community. 
@@ -21,11 +21,12 @@ export default function Expressions() {
           </>
         </section>
 
-        <section className="m-auto relative max-w-[1140px] flex flex-col items-center">
+        {/* <section className="m-auto relative max-w-[1140px] flex flex-col items-center">
           <ExpressionsIntro />
-        </section>
+        </section> */}
 
-        <section className="mt-10 lg:mt-36">
+        {/* <section className="mt-10 lg:mt-36"> */}
+        <section className="mt-10 lg:mt-15">
           <MainExpressions 
             tabs={expressions}
             selectedTab={expression}
